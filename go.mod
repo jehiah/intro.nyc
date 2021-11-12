a79af106d54ecdfeb9c1d8a51f78bc49eb7bc09b
Jehiah Czebotar <jehiah@gmail.com>
2021-11-12 08:52:05 -0500

    mvp: redirect to legistar.council.nyc.gov

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/jehiah/intro.nyc
 
 go 1.17
+
+require (
+	github.com/felixge/httpsnoop v1.0.1 // indirect
+	github.com/gorilla/handlers v1.5.1 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+)
--- intro_nyc.go
+++ intro_nyc.go
@@ -1,34 +1,47 @@
 package main
 
 import (
-        "fmt"
+        "flag"
         "log"
         "net/http"
         "os"
+
+        "github.com/gorilla/handlers"
+        "github.com/julienschmidt/httprouter"
 )
 
+type App struct {
+}
+
+func (a *App) Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+        http.Redirect(w, r, "https://legistar.council.nyc.gov/Legislation.aspx", 302)
+}
+
 func main() {
+        logRequests := flag.Bool("log-requests", false, "log requests")
+        flag.Parse()
+
         log.Print("starting server...")
-        http.HandleFunc("/", handler)
+
+        app := &App{}
+
+        router := httprouter.New()
+        router.GET("/", app.Index)
 
         // Determine port for HTTP service.
         port := os.Getenv("PORT")
         if port == "" {
                 port = "8080"
-                log.Printf("defaulting to port %s", port)
+        }
+
+        var h http.Handler = router
+        if *logRequests {
+                h = handlers.LoggingHandler(os.Stdout, h)
         }
 
         // Start HTTP server.
         log.Printf("listening on port %s", port)
-        if err := http.ListenAndServe(":"+port, nil); err != nil {
+        if err := http.ListenAndServe(":"+port, h); err != nil {
                 log.Fatal(err)
         }
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-        name := os.Getenv("NAME")
-        if name == "" {
-                name = "World"
-        }
-        fmt.Fprintf(w, "Hello %s!\n", name)
 }
