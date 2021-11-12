bc3dae7978139d2b9748580ee65fcff712578d2f
Jehiah Czebotar <jehiah@gmail.com>
2021-11-12 09:51:39 -0500

    intro.nyc: implement redirect pattern

--- go.mod
+++ go.mod
@@ -3,7 +3,14 @@ module github.com/jehiah/intro.nyc
 go 1.17
 
 require (
-	github.com/felixge/httpsnoop v1.0.1 // indirect
-	github.com/gorilla/handlers v1.5.1 // indirect
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/gorilla/handlers v1.5.1
+	github.com/jehiah/legislator v0.0.0-20211112144850-03783eea0671
+	github.com/julienschmidt/httprouter v1.3.0
+)
+
+require (
+	github.com/felixge/httpsnoop v1.0.2 // indirect
+	github.com/gosimple/slug v1.11.2 // indirect
+	github.com/gosimple/unidecode v1.0.1 // indirect
+	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
 )
--- intro_nyc.go
+++ intro_nyc.go
@@ -2,19 +2,83 @@ package main
 
 import (
         "flag"
+        "fmt"
         "log"
         "net/http"
+        "net/url"
         "os"
+        "regexp"
+        "strconv"
+        "strings"
 
         "github.com/gorilla/handlers"
+        "github.com/jehiah/legislator/legistar"
         "github.com/julienschmidt/httprouter"
 )
 
 type App struct {
+        legistar *legistar.Client
+
+        cachedRedirects map[string]string
 }
 
 func (a *App) Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
         http.Redirect(w, r, "https://legistar.council.nyc.gov/Legislation.aspx", 302)
+}
+
+func (a *App) FileRedirect(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+        file := ps.ByName("file")
+        if ok, _ := regexp.MatchString("^[0-9]{4}-20(14|15|16|17|18|19|20|21|22)", file); !ok {
+                http.Error(w, "Not Found", 404)
+                return
+        }
+
+        n := strings.Split(file, "-")
+        seq, _ := strconv.Atoi(n[0])
+        if seq > 3000 {
+                http.Error(w, "Not Found", 404)
+                return
+        }
+        // year, err := strconv.Atoi(n[1])
+        // if err != nil {
+        //         log.Print(err)
+        //         http.Error(w, "INVALID REQUEST", 400)
+        //         return
+        // }
+        file = fmt.Sprintf("Int %s", file)
+
+        if redirect, ok := a.cachedRedirects[file]; ok {
+                http.Redirect(w, r, redirect, 302)
+                return
+        }
+
+        filter := legistar.AndFilters(
+                legistar.MatterTypeFilter("Introduction"),
+                legistar.MatterFileFilter(file),
+        )
+
+        matters, err := a.legistar.Matters(r.Context(), filter)
+        if err != nil {
+                log.Print(err)
+                http.Error(w, "unknown error", 500)
+                return
+        }
+        if len(matters) != 1 {
+                // TODO: cache?
+                http.Error(w, "Not Found", 404)
+                return
+        }
+
+        // we have one
+        redirect, err := a.legistar.LookupWebURL(r.Context(), matters[0].ID)
+        if err != nil {
+                log.Print(err)
+                http.Error(w, "unknown error", 500)
+                return
+        }
+        a.cachedRedirects[file] = redirect
+
+        http.Redirect(w, r, redirect, 302)
 }
 
 func main() {
@@ -23,10 +87,20 @@ func main() {
 
         log.Print("starting server...")
 
-        app := &App{}
+        app := &App{
+                legistar: legistar.NewClient("nyc", os.Getenv("NYC_LEGISLATOR_TOKEN")),
+
+                cachedRedirects: make(map[string]string),
+        }
+        var err error
+        app.legistar.LookupURL, err = url.Parse("https://legistar.council.nyc.gov/gateway.aspx?m=l&id=")
+        if err != nil {
+                panic(err)
+        }
 
         router := httprouter.New()
         router.GET("/", app.Index)
+        router.GET("/:file", app.FileRedirect)
 
         // Determine port for HTTP service.
         port := os.Getenv("PORT")
