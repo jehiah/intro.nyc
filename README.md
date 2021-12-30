f0a581f19b846b8952290a62edee11402a845e3f
Jehiah Czebotar <jehiah@gmail.com>
2021-12-30 16:27:53 -0500

    add council member detail pages

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 
-Quick Search and Short Links for NYC Council Legislation
+NYC Council Legislation UI
+
+Data from [NYC Council Legislative API](https://council.nyc.gov/legislation/api/)  as archived on Github [nyc_legislation](https://github.com/jehiah/nyc_legislation)
+
+intro.nyc is a Legislation search and research tool designed to make legislation more accessible to constituents and to increase Council Member accountability by transparency
+
+https://intro.nyc/
 
 ### Quick Search
 
 Link to searches with `/?q=${query}`. In-browser searching is implemented with [flexsearch](https://github.com/nextapps-de/flexsearch) based on data from [`jehiah/nyc_legislation`](https://github.com/jehiah/nyc_legislation)
 
-### Short Links
+### Links
 
 `https://intro.nyc/${intro_number}-${intro_year}`
 i.e. https://intro.nyc/2159-2020
@@ -16,4 +22,8 @@ i.e. https://intro.nyc/1394-2019/local-law
 `https://intro.nyc/local-laws` and `https://intro.nyc/local-laws/$year`
 i.e. https://intro.nyc/local-laws/2021
 
+`https://intro.nyc/councilmembers` and `https://intro.nyc/councilmembers/$name` i.e. https://intro.nyc/councilmembers/tiffany-caban
 
+### Questions? Suggestions?
+
+Open an Issue
--- intro_nyc.go
+++ intro_nyc.go
@@ -301,6 +301,194 @@ func (a *App) Councilmembers(w http.ResponseWriter, r *http.Request, ps httprout
 	}
 }
 
+type Legislation struct {
+	All []db.Legislation
+}
+
+func (l Legislation) FilterPrimarySponsor(sponsor int) Legislation {
+	var o []db.Legislation
+	for _, ll := range l.All {
+		if len(ll.Sponsors) > 0 && ll.Sponsors[0].ID == sponsor {
+			o = append(o, ll)
+		}
+	}
+	return Legislation{All: o}
+}
+func (l Legislation) FilterSecondarySponsor(sponsor int) Legislation {
+	var o []db.Legislation
+	for _, ll := range l.All {
+		if len(ll.Sponsors) > 1 {
+			for _, s := range ll.Sponsors[1:] {
+				if s.ID == sponsor {
+					o = append(o, ll)
+				}
+			}
+		}
+	}
+	return Legislation{All: o}
+}
+
+func (l Legislation) Statuses() []Status {
+	d := make(map[string]int)
+	for _, ll := range l.All {
+		d[ll.StatusName] += 1
+	}
+	var o []Status
+	for n, c := range d {
+		o = append(o, Status{Name: n, Count: c, Percent: (float64(c) / float64(len(l.All))) * 100})
+	}
+	sortSeq := []string{
+		// exhaustive from MatterStatuses
+		"Adopted",
+		"Approved",
+		"Companion Pending Approval by Council",
+		"Coupled on Call-Up Vote",
+		"Defeated",
+		"Deferred",
+		"Disapproved",
+		"Discharged from Committee",
+		"Failed",
+		"Filed",
+		"General Orders Calendar",
+		"Hearing Transcripts ",
+		"Local Laws",
+		"Press Conference Filed",
+		"Press Conference Scheduled",
+		"Received, Ordered, Printed and Filed",
+		"Reported from Committee and Introduced",
+		"Reported from Committee",
+		"Returned Unsigned by Mayor",
+		"Special Event Filed",
+		"Special Event Scheduled",
+		"Town Hall Meeting Filed",
+		"Town Hall Meeting Scheduled",
+
+		"Introduced",
+		"Committee",
+		"Laid Over in Committee",
+		"Filed (End of Session)",
+
+		"Vetoed",
+		"Withdrawn",
+		"Enacted (Charter Referendum)",
+		"Enacted (Mayor's Desk for Signature)",
+		"Enacted",
+	}
+	sortLookup := make(map[string]int, len(sortSeq))
+	for i, s := range sortSeq {
+		sortLookup[s] = i
+	}
+
+	sort.Slice(o, func(i, j int) bool { return sortLookup[o[i].Name] < sortLookup[o[j].Name] })
+
+	// TODO: sort by sequence not by string
+	return o
+}
+
+type Status struct {
+	Name    string
+	Count   int
+	Percent float64
+}
+
+func (s Status) CSSClass() string {
+	return "status-" + strings.ToLower(strings.Fields(s.Name)[0])
+}
+
+// Councilmember returns the list of councilmembers at /councilmembers/$name
+func (a *App) Councilmember(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	councilmember := ps.ByName("year")
+	log.Printf("Councilmember %q", councilmember)
+	if matched, err := regexp.MatchString("^[a-z-]+$", councilmember); err != nil {
+		log.Print(err)
+		http.Error(w, "Internal Server Error", 500)
+		return
+	} else if !matched {
+		log.Printf("council member %q not found", councilmember)
+		http.Error(w, "Not Found", 404)
+		return
+	}
+
+	t := newTemplate(a.templateFS, "councilmember.html")
+
+	var people []db.Person
+	err := a.getJSONFile(r.Context(), "build/people_active.json", &people)
+	if err != nil {
+		if err == storage.ErrObjectNotExist {
+			a.addExpireHeaders(w, time.Minute*5)
+			http.Error(w, "Not Found", 404)
+			return
+		}
+		log.Print(err)
+		http.Error(w, "Internal Server Error", 500)
+	}
+	var person db.Person
+	for _, p := range people {
+		if p.Slug == councilmember {
+			person = p
+			break
+		}
+	}
+	if person.Slug == "" {
+		log.Printf("council member %q not found", councilmember)
+		a.addExpireHeaders(w, time.Minute*5)
+		http.Error(w, "Not Found", 404)
+		return
+	}
+
+	cacheTTL := time.Minute
+
+	type Page struct {
+		Page             string
+		Person           Person
+		LastSync         LastSync
+		Legislation      Legislation
+		PrimarySponsor   Legislation
+		SecondarySponsor Legislation
+	}
+	body := Page{
+		Page:   "councilmembers",
+		Person: Person{Person: person},
+	}
+
+	// var twitter []Twitter
+	// err = a.getJSONFile(r.Context(), "build/twitter.json", &twitter)
+	// if err != nil {
+	// 	log.Print(err)
+	// 	http.Error(w, "Internal Server Error", 500)
+	// }
+	// for _, t := range twitter {
+	// 	if t.ID == body.Person.Person.ID {
+	// 		body.Person.Twitter = t
+	// 	}
+	// }
+
+	err = a.getJSONFile(r.Context(), fmt.Sprintf("build/legislation_%s.json", person.Slug), &body.Legislation.All)
+	if err != nil {
+		// not found is ok; it means they are likely not active in current session (yet?)
+		if err != storage.ErrObjectNotExist {
+			log.Print(err)
+			http.Error(w, "Internal Server Error", 500)
+		}
+	}
+	body.PrimarySponsor = body.Legislation.FilterPrimarySponsor(person.ID)
+	body.SecondarySponsor = body.Legislation.FilterSecondarySponsor(person.ID)
+
+	err = a.getJSONFile(r.Context(), "build/last_sync.json", &body.LastSync)
+	if err != nil {
+		log.Print(err)
+		http.Error(w, "Internal Server Error", 500)
+	}
+
+	w.Header().Set("content-type", "text/html")
+	a.addExpireHeaders(w, cacheTTL)
+	err = t.ExecuteTemplate(w, "councilmember.html", body)
+	if err != nil {
+		log.Print(err)
+		http.Error(w, "Internal Server Error", 500)
+	}
+}
+
 func (a *App) getJSONFile(ctx context.Context, filename string, v interface{}) error {
 	f, err := a.getFile(ctx, filename)
 	if err != nil {
@@ -311,6 +499,7 @@ func (a *App) getJSONFile(ctx context.Context, filename string, v interface{}) e
 }
 
 func (a *App) getFile(ctx context.Context, filename string) (io.ReadCloser, error) {
+	log.Printf("get gs://intronyc/%s", filename)
 	return a.gsclient.Bucket("intronyc").Object(filename).NewReader(ctx)
 }
 
@@ -322,6 +511,7 @@ func (a *App) addExpireHeaders(w http.ResponseWriter, duration time.Duration) {
 	w.Header().Add("Expires", time.Now().Add(duration).Format(http.TimeFormat))
 }
 
+// L2 /:file/:year
 func (a *App) L2(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	path := ps.ByName("year")
 	file := ps.ByName("file")
@@ -333,11 +523,20 @@ func (a *App) L2(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		a.LocalLaws(w, r, ps)
 		return
 	}
+	if file == "councilmembers" {
+		a.Councilmember(w, r, ps)
+		return
+	}
 	if file == "static" {
 		a.staticHandler.ServeHTTP(w, r)
 		return
 	}
-	a.IntroJSON(w, r, ps)
+	if file == "data" {
+		a.IntroJSON(w, r, ps)
+		return
+	}
+	http.Error(w, "Not Found", 404)
+	return
 }
 
 // IntroJSON proxies to /data/${year}.json to github:jehiah/nyc_legislation:introduction/$year/index.json
@@ -345,14 +544,6 @@ func (a *App) L2(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 // Note: the router match pattern is `/:file/:year` so `:file` must be == "data"
 func (a *App) IntroJSON(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	path := ps.ByName("year")
-	file := ps.ByName("file")
-	switch file {
-	case "data":
-	default:
-		log.Printf("file != data %q", file)
-		http.Error(w, "Not Found", 404)
-		return
-	}
 
 	if !strings.HasSuffix(path, ".json") {
 		log.Printf("year %q", path)
