7d0dbf3ce92cde362fcf26f9203824ca433d57ba
Jehiah Czebotar <jehiah@gmail.com>
2022-08-09 21:06:36 -0400

    add /reports/status

--- reports.go
+++ reports.go
@@ -19,6 +19,8 @@ func (a *App) Reports(w http.ResponseWriter, r *http.Request, ps httprouter.Para
 		http.Redirect(w, r, "/reports/most_sponsored", 302)
 	case "most_sponsored":
 		a.ReportMostSponsored(w, r)
+	case "status":
+		a.ReportByStatus(w, r)
 	default:
 		http.Error(w, "Not Found", 404)
 	}
@@ -35,11 +37,11 @@ func (a *App) ReportMostSponsored(w http.ResponseWriter, r *http.Request) {
 		LastSync    LastSync
 		Legislation LegislationList
 		Committees  []string
-		Sessions    []Session
+		// Sessions    []Session
 	}
 	body := Page{
-		Page:     "reports",
-		Sessions: Sessions[:3],
+		Page: "reports",
+		// Sessions: Sessions[:3],
 	}
 
 	err := a.getJSONFile(r.Context(), "build/last_sync.json", &body.LastSync)
@@ -76,6 +78,124 @@ func (a *App) ReportMostSponsored(w http.ResponseWriter, r *http.Request) {
 		body.Committees = append(body.Committees, strings.TrimPrefix(b, "Committee on "))
 	}
 	sort.Strings(body.Committees)
+
+	w.Header().Set("content-type", "text/html")
+	cacheTTL := time.Minute * 15
+	a.addExpireHeaders(w, cacheTTL)
+	err = t.ExecuteTemplate(w, template, body)
+	if err != nil {
+		log.Print(err)
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+}
+
+// ReportByStatus shows the change in bill status over time
+func (a *App) ReportByStatus(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	template := "report_by_status.html"
+
+	t := newTemplate(a.templateFS, template)
+
+	type Row struct {
+		Date   string
+		Count  int
+		Status string
+		Time   time.Time `json:"-"`
+		Last   bool      `json:"Last,omitempty"`
+	}
+	type Page struct {
+		Page     string
+		SubPage  string
+		LastSync LastSync
+		Data     []Row
+		Session  Session
+		Sessions []Session
+	}
+	body := Page{
+		Page:     "reports",
+		SubPage:  "by_status",
+		Session:  CurrentSession,
+		Sessions: Sessions,
+	}
+	for _, s := range Sessions {
+		if s.String() == r.Form.Get("session") {
+			body.Session = s
+		}
+	}
+
+	introduced, hearing, approved, enacted := make(map[time.Time]int), make(map[time.Time]int), make(map[time.Time]int), make(map[time.Time]int)
+
+	err := a.getJSONFile(r.Context(), "build/last_sync.json", &body.LastSync)
+	if err != nil {
+		log.Print(err)
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	// get all the years for the legislative session
+	for year := body.Session.StartYear; year <= body.Session.EndYear && year <= time.Now().Year(); year++ {
+		var l []Legislation
+		err := a.getJSONFile(r.Context(), fmt.Sprintf("build/%d.json", year), &l)
+		if err != nil {
+			if err == storage.ErrObjectNotExist {
+				continue
+			}
+			log.Print(err)
+			http.Error(w, "Internal Server Error", 500)
+			return
+		}
+		for _, ll := range l {
+			seen := make(map[string]bool)
+			for _, h := range ll.History {
+				if seen[h.Action] {
+					continue
+				}
+				seen[h.Action] = true // only track first hearing
+				day := h.Date.Truncate(time.Hour * 24)
+
+				switch h.Action {
+				case "Introduced by Council":
+					introduced[day] = introduced[day] + 1
+				case "Hearing Held by Committee":
+					hearing[day] = hearing[day] + 1
+				case "Approved by Council":
+					approved[day] = approved[day] + 1
+				case "City Charter Rule Adopted", "Signed Into Law by Mayor":
+					enacted[day] = enacted[day] + 1
+				default:
+					continue
+				}
+			}
+		}
+	}
+
+	today := time.Now().Truncate(time.Hour * 24)
+	for i, d := range []map[time.Time]int{introduced, hearing, approved, enacted} {
+		status := []string{"Introduced", "Hearing Held", "Approved", "Enacted"}[i]
+		var data []Row
+		for date, count := range d {
+			data = append(data, Row{Time: date, Date: date.Format("2006-01-02"), Count: count, Status: status})
+		}
+		sort.Slice(data, func(i, j int) bool { return data[i].Time.Before(data[j].Time) })
+		carry := 0
+		for i, v := range data {
+			data[i].Count += carry
+			carry += v.Count
+		}
+
+		if body.Session == CurrentSession {
+			// add current values as 'today'
+			last := data[len(data)-1]
+			if !last.Time.Equal(today) {
+				data = append(data, Row{Time: today, Date: today.Format("2006-01-02"), Count: last.Count, Status: last.Status})
+			}
+		}
+		data[len(data)-1].Last = true
+
+		body.Data = append(body.Data, data...)
+
+	}
 
 	w.Header().Set("content-type", "text/html")
 	cacheTTL := time.Minute * 15
