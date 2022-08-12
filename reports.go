257371ef02a74ebb2c8e6e96f6dca598158e9d9b
Jehiah Czebotar <jehiah@gmail.com>
2022-08-11 22:59:00 -0400

    reports: updates

--- reports.go
+++ reports.go
@@ -9,7 +9,9 @@ import (
 	"time"
 
 	"cloud.google.com/go/storage"
+	"github.com/jehiah/legislator/db"
 	"github.com/julienschmidt/httprouter"
+	// "gonum.org/v1/gonum/mat"
 )
 
 // Reports handles /reports/...
@@ -21,6 +23,8 @@ func (a *App) Reports(w http.ResponseWriter, r *http.Request, ps httprouter.Para
 		a.ReportMostSponsored(w, r)
 	case "status":
 		a.ReportByStatus(w, r)
+	case "similarity":
+		a.ReportSimilarity(w, r)
 	default:
 		http.Error(w, "Not Found", 404)
 	}
@@ -146,6 +150,12 @@ func (a *App) ReportByStatus(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		for _, ll := range l {
+			if ll.StatusName == "Withdrawn" {
+				continue
+			}
+			day := ll.IntroDate.Truncate(time.Hour * 24)
+			introduced[day] = introduced[day] + 1
+
 			seen := make(map[string]bool)
 			for _, h := range ll.History {
 				if seen[h.Action] {
@@ -155,8 +165,9 @@ func (a *App) ReportByStatus(w http.ResponseWriter, r *http.Request) {
 				day := h.Date.Truncate(time.Hour * 24)
 
 				switch h.Action {
-				case "Introduced by Council":
-					introduced[day] = introduced[day] + 1
+				// use IntroDate directly; some bills don't have a matching action 0407-2022
+				// case "Introduced by Council":
+				// 	introduced[day] = introduced[day] + 1
 				case "Hearing Held by Committee":
 					hearing[day] = hearing[day] + 1
 				case "Approved by Council":
@@ -209,4 +220,122 @@ func (a *App) ReportByStatus(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Internal Server Error", 500)
 		return
 	}
+}
+
+// ReportSimilarity shows how similar CMs are
+func (a *App) ReportSimilarity(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	template := "report_similarity.html"
+
+	t := newTemplate(a.templateFS, template)
+
+	type Row struct {
+		A, B  string
+		Count int
+	}
+
+	type Page struct {
+		Page     string
+		SubPage  string
+		LastSync LastSync
+		Data     []Row
+		Session  Session
+		Sessions []Session
+		Matrix   [][]int
+		Names    []string
+	}
+	body := Page{
+		Page:     "reports",
+		SubPage:  "by_status",
+		Session:  CurrentSession,
+		Sessions: Sessions,
+	}
+	for _, s := range Sessions {
+		if s.String() == r.Form.Get("session") {
+			body.Session = s
+		}
+	}
+
+	err := a.getJSONFile(r.Context(), "build/last_sync.json", &body.LastSync)
+	if err != nil {
+		log.Print(err)
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	cmLookup := make(map[string]int)
+	cm := func(p string) int {
+		p = strings.TrimSpace(p)
+		if n, ok := cmLookup[p]; ok {
+			return n
+		}
+		n := len(cmLookup)
+		cmLookup[p] = n
+		return n
+	}
+
+	var sponsors [][]db.PersonReference
+
+	// get all the years for the legislative session
+	for year := body.Session.StartYear; year <= body.Session.EndYear && year <= time.Now().Year(); year++ {
+		var l []Legislation
+		err := a.getJSONFile(r.Context(), fmt.Sprintf("build/%d.json", year), &l)
+		if err != nil {
+			if err == storage.ErrObjectNotExist {
+				continue
+			}
+			log.Print(err)
+			http.Error(w, "Internal Server Error", 500)
+			return
+		}
+		for _, ll := range l {
+			sponsors = append(sponsors, ll.Sponsors)
+			for _, s := range ll.Sponsors {
+				cm(s.FullName)
+			}
+		}
+	}
+
+	matrix := make([][]int, len(cmLookup))
+	for i := range matrix {
+		matrix[i] = make([]int, len(cmLookup))
+	}
+	lookup := make([]string, len(cmLookup))
+	for fullName, i := range cmLookup {
+		lookup[i] = fullName
+	}
+
+	for _, s := range sponsors {
+		for _, a := range s {
+			if strings.TrimSpace(a.FullName) == "" {
+				continue
+			}
+			for _, b := range s {
+				if strings.TrimSpace(b.FullName) == "" {
+					continue
+				}
+				i, j := cm(a.FullName), cm(b.FullName)
+				matrix[i][j] += 1
+				if i != j {
+					matrix[j][i] += 1
+				}
+			}
+		}
+	}
+	for i, m := range matrix {
+		for j, v := range m {
+			body.Data = append(body.Data, Row{A: lookup[i], B: lookup[j], Count: v})
+		}
+	}
+
+	w.Header().Set("content-type", "text/html")
+	cacheTTL := time.Minute * 15
+	a.addExpireHeaders(w, cacheTTL)
+	err = t.ExecuteTemplate(w, template, body)
+	if err != nil {
+		log.Print(err)
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
 }
