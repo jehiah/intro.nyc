f6f30e6c3f422bc1d94c650e6b7f487ad8a8bb25
Jehiah Czebotar <jehiah@gmail.com>
2022-10-11 23:14:19 -0400

    reports: add council member report

--- reports.go
+++ reports.go
@@ -25,6 +25,8 @@ func (a *App) Reports(w http.ResponseWriter, r *http.Request, ps httprouter.Para
 		a.ReportByStatus(w, r)
 	case "similarity":
 		a.ReportSimilarity(w, r)
+	case "councilmembers":
+		a.ReportCouncilmembers(w, r)
 	default:
 		http.Error(w, "Not Found", 404)
 	}
@@ -336,4 +338,148 @@ func (a *App) ReportSimilarity(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+}
+
+// ReportCouncilmembers shows the legislative activity of each councilmember
+func (a *App) ReportCouncilmembers(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	template := "report_by_councilmember.html"
+
+	t := newTemplate(a.templateFS, template)
+
+	type Row struct {
+		Person db.PersonReference
+
+		IntroIntro   int
+		IntroHearing int
+		IntroPassed  int
+		IntroEnacted int
+		IntroVeto    int
+
+		SponsorIntro   int
+		SponsorHearing int
+		SponsorPassed  int
+		SponsorEnacted int
+		SponsorVeto    int
+	}
+	type Page struct {
+		Page     string
+		SubPage  string
+		LastSync LastSync
+		Data     []Row
+		Session  Session
+		Sessions []Session
+	}
+	body := Page{
+		Page:     "reports",
+		SubPage:  "by_councilmember",
+		Session:  CurrentSession,
+		Sessions: Sessions,
+	}
+	for _, s := range Sessions {
+		if s.String() == r.Form.Get("session") {
+			body.Session = s
+		}
+	}
+
+	err := a.getJSONFile(r.Context(), "build/last_sync.json", &body.LastSync)
+	if err != nil {
+		log.Print(err)
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	data := make(map[string]*Row)
+
+	// get all the years for the legislative session
+	for year := body.Session.StartYear; year <= body.Session.EndYear && year <= time.Now().Year(); year++ {
+		var l []Legislation
+		err := a.getJSONFile(r.Context(), fmt.Sprintf("build/%d.json", year), &l)
+		if err != nil {
+			if err == storage.ErrObjectNotExist {
+				continue
+			}
+			log.Print(err)
+			http.Error(w, "Internal Server Error", 500)
+			return
+		}
+		for _, ll := range l {
+			if ll.StatusName == "Withdrawn" {
+				continue
+			}
+
+			var hasHearing, hasPassed, hasEnacted bool
+
+			seen := make(map[string]bool)
+			for _, h := range ll.History {
+				if seen[h.Action] {
+					continue
+				}
+				seen[h.Action] = true // only track first hearing
+				switch h.Action {
+				case "Hearing Held by Committee", "Hearing on P-C Item by Comm":
+					hasHearing = true
+				case "Approved by Council":
+					hasPassed = true
+				case "City Charter Rule Adopted", "Signed Into Law by Mayor",
+					"Overridden by Council": // possible after "Vetoed by Mayor" (See Int 1208-2013)
+					hasEnacted = true
+				default:
+					continue
+				}
+			}
+			for i, s := range ll.Sponsors {
+				r, ok := data[s.FullName]
+				if !ok {
+					r = &Row{Person: s}
+					data[s.FullName] = r
+				}
+				if i == 0 {
+					r.IntroIntro += 1
+					if hasHearing {
+						r.IntroHearing += 1
+					}
+					if hasPassed {
+						r.IntroPassed += 1
+					}
+					if hasEnacted {
+						r.IntroEnacted += 1
+					}
+					// TODO veto?
+				} else {
+					r.SponsorIntro += 1
+					if hasHearing {
+						r.SponsorHearing += 1
+					}
+					if hasPassed {
+						r.SponsorPassed += 1
+					}
+					if hasEnacted {
+						r.SponsorEnacted += 1
+					}
+				}
+			}
+		}
+	}
+
+	for _, r := range data {
+		body.Data = append(body.Data, *r)
+	}
+	sort.Slice(body.Data, func(i, j int) bool {
+		if body.Data[i].IntroIntro == body.Data[j].IntroIntro {
+			return strings.Compare(body.Data[i].Person.FullName, body.Data[j].Person.FullName) == -1
+		}
+
+		return body.Data[i].IntroIntro > body.Data[j].IntroIntro
+	})
+
+	w.Header().Set("content-type", "text/html")
+	cacheTTL := time.Minute * 15
+	a.addExpireHeaders(w, cacheTTL)
+	err = t.ExecuteTemplate(w, template, body)
+	if err != nil {
+		log.Print(err)
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
 }
